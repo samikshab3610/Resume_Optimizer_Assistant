5b720f1eb70076949fa719e9cf43e7b7ae08073d
Samiksha Bhore <samikshabhore7636@gmail.com>
2026-08-01 18:59:33 +0530

    Add global error handler and 404 handler

--- server/src/app.js
+++ server/src/app.js
@@ -41,4 +41,16 @@ app.get("/api/health", (req, res) => {
   });
 });
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || "Something went wrong. Please try again.",
+  });
+});
+
+
 module.exports = app;
