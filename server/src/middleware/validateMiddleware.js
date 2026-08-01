747b963ebd93cea314c61c0b0e61632c75caa89d
Samiksha Bhore <samikshabhore7636@gmail.com>
2026-08-01 12:56:26 +0530

    Add zod validation for auth routes

---
+++ server/src/middleware/validateMiddleware.js
@@ -0,0 +1,14 @@
+const validate = (schema) => (req, res, next) => {
+  const result = schema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({
+      message: result.error.issues[0].message,
+    });
+  }
+
+  req.body = result.data;
+  next();
+};
+
+module.exports = validate;
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { signup, login, getMe } = require("../controllers/authController");
 const protect = require("../middleware/authMiddleware");
+const validate = require("../middleware/validateMiddleware");
+const { signupSchema, loginSchema } = require("../validators/authValidators");
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", validate(signupSchema), signup);
+router.post("/login", validate(loginSchema), login);
 router.get("/me", protect, getMe);
 
 module.exports = router;
